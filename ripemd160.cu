7a8a3745c54ec672e363c68c2ee58c1d89389367
Soumya001 <85055445+Soumya001@users.noreply.github.com>
2025-06-23 09:35:42 +0530

    Update ripemd160.cu

--- ripemd160.cu
+++ ripemd160.cu
@@ -1,3 +1,5 @@
-__device__ void ripemd160(const uint8_t* data, size_t len, uint8_t* out20) {
-    for (int i = 0; i < 20; i++) out20[i] = data[i % len] ^ (i * 31);
+extern "C" __device__ void ripemd160(const uint8_t* data, size_t len, uint8_t* out20) {
+    for (int i = 0; i < 20; i++) {
+        out20[i] = data[i % len] ^ (i * 31);
+    }
 }
