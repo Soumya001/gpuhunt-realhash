b2043ce4b2fc66822f05a8192252ad45e998276f
Soumya001 <85055445+Soumya001@users.noreply.github.com>
2025-06-23 09:22:07 +0530

    Update gpu_scan.cuh

--- gpu_scan.cuh
+++ gpu_scan.cuh
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <array>
+#include <stdint.h>
+
 std::vector<std::pair<uint64_t, std::array<uint8_t, 20>>> scan_range_on_gpu_with_output(
     uint64_t start,
     uint64_t end,
