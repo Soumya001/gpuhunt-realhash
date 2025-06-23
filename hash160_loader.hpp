d1c9c3e52f8cd39f8c9e2a15a5540f67dde81d00
Soumya001 <85055445+Soumya001@users.noreply.github.com>
2025-06-23 09:16:38 +0530

    Update hash160_loader.hpp

--- hash160_loader.hpp
+++ hash160_loader.hpp
@@ -1,18 +1,12 @@
-#ifndef HASH160_LOADER_HPP
-#define HASH160_LOADER_HPP
+#pragma once
 #include <vector>
 #include <array>
 #include <fstream>
 
-bool load_hash160_bin(const std::string& filename, std::vector<std::array<uint8_t, 20>>& hashes) {
+inline bool load_hash160_bin(const std::string& filename, std::vector<std::array<uint8_t, 20>>& out) {
     std::ifstream f(filename, std::ios::binary);
     if (!f) return false;
-
     std::array<uint8_t, 20> buf;
-    while (f.read(reinterpret_cast<char*>(buf.data()), 20)) {
-        hashes.push_back(buf);
-    }
+    while (f.read((char*)buf.data(), 20)) out.push_back(buf);
     return true;
 }
-
-#endif
