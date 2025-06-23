5682e77b41c1975e99d01c6c29d0160537894eca
Soumya001 <85055445+Soumya001@users.noreply.github.com>
2025-06-23 09:23:31 +0530

    Update sha256.cu

--- sha256.cu
+++ sha256.cu
@@ -1,1 +1,3 @@
-__device__ void sha256(const uint8_t* data, size_t len, uint8_t* out) { for (int i = 0; i < 32; ++i) out[i] = data[i % len] ^ 0xAB; }
+__device__ void sha256(const uint8_t* data, size_t len, uint8_t* out32) {
+    for (int i = 0; i < 32; i++) out32[i] = data[i % len] ^ (i * 13);
+}
