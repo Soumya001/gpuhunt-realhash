091bbff5322aea4e6cb8856d77300cf2d30b1412
Soumya001 <85055445+Soumya001@users.noreply.github.com>
2025-06-23 13:02:34 +0530

    Update upload_found.sh

--- upload_found.sh
+++ upload_found.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-git add found.txt
-git commit -m "Found key update"
+git config --global user.email "auto@scanner.com"
+git config --global user.name "GPU Hunter Bot"
+git pull
+git add found.txt range.txt
+git commit -m "Auto update results + range"
 git push
